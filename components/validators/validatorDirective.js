bd893fcf8c85a52c3714b2353d1ab3f650331818
Dirfb612 <dianita.a2e@gmailcom>
2015-10-27 11:04:06 -0500

    examples to create a validators

--- components/validators/validatorDirective.js
+++ components/validators/validatorDirective.js
@@ -1,1 +1,25 @@
+angular
+   .module('app')
+   .directive('changeCase', function () {
+   return {
+      restrict: 'A',
+     // templateUrl: 'components/validators/ejemploValidatorTemplate.html',
+      require: 'ngModel',
+      link: function (scope, element, attr, ngModel) {
 
+         ngModel.$formatters.push(function(value){
+            value.toUpperCase();
+            return value;
+         });
+
+         /*ngModel.$parsers.push(function (value) {
+
+         });*/
+         ngModel.$parsers.push(function(value){
+            value.toUpperCase();
+            return value;
+         });
+
+      }
+   };
+});
